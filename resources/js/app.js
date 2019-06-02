257dda658462869c413d55f8f3e5500a5ce965db
Granite Bagas <granitebagas28@gmail.com>
2019-06-02 20:28:11 +0700

    add function to create user

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,15 +8,25 @@ require("./bootstrap");
 
 window.Vue = require("vue");
 
+import { Form, HasError, AlertError } from "vform";
 import VueRouter from "vue-router";
+
 Vue.use(VueRouter);
+
+window.Form = Form;
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
 
 let routes = [
     {
         path: "/dashboard",
         component: require("./components/Dashboard.vue").default
     },
-    { path: "/profile", component: require("./components/Profile.vue").default }
+    {
+        path: "/profile",
+        component: require("./components/Profile.vue").default
+    },
+    { path: "/users", component: require("./components/Users.vue").default }
 ];
 
 const router = new VueRouter({
